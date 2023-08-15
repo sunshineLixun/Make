f79cd6317ebcdeca5168af54156fd41623b8e0e7
Hello Li <1261142605@qq.com>
2023-08-15 10:11:23 +0800

    feat(main): 🚀 add progress

--- src/main/utils/yt-dlp.ts
+++ src/main/utils/yt-dlp.ts
@@ -1,13 +1,20 @@
 import youtubedl from "youtube-dl-exec";
+import createLogger from "progress-estimator";
+
+const logger = createLogger();
 
 export function downloadYt() {
-  youtubedl("https://www.youtube.com/watch?v=ISnxs-NlRYg", {
+  const promise = youtubedl("https://www.youtube.com/watch?v=ISnxs-NlRYg", {
     writeAutoSub: true,
     writeSub: true,
     convertSubs: "srt",
-    output: process.cwd(),
-    skipDownload: true
-  })
-    .then(output => console.log(output))
-    .catch(err => console.log(err));
+    output: process.cwd() + "/resources/subtitle",
+    format: "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best"
+  });
+
+  logger(promise, "下载中...", {
+    estimate: 1000 * 60 * 5
+  }).then(res => {
+    console.log(res);
+  });
 }
